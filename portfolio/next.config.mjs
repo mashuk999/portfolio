41502adb5eb020851876e84a513491d582aefd4f
Abhay Nigam <36666689+mashuk999@users.noreply.github.com>
2024-04-09 16:22:31 +0000

    Configured Next JS to make it compatible with Github Pages

--- portfolio/next.config.mjs
+++ portfolio/next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    basePath: "/portfolio",
+    output: 'export' // Added Export so to generate Static HTML CSS so portfolio can be run on Github Pages
+};
 
 export default nextConfig;
